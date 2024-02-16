cfea24826df18efdcfd9b62fea6b5b4ce38410e2
Greg <119018335+wkl761212@users.noreply.github.com>
2024-02-15 21:01:32 -0700

    Remove Testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/about/about'
 import Experience from './components/experience/experience'
 import Services from './components/services/services'
 import Portfolio from './components/portfolio/portfolio'
-import Testimonials from './components/testimonials/testimonials'
+
 import Contact from './components/contact/contact'
 import Footer from './components/footer/footer'
 
@@ -18,7 +18,7 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
+      
       <Contact />
       <Footer />
     </>
